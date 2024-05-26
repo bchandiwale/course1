c4ff1fc9854862ffb41174249682f908c473d6a7
bhavesh_chandiwale <bhavesh_chandiwale@persistent.com>
2024-05-26 15:50:38 +0530

    first commit

---
+++ angular/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa92f9jbr6oCBshT5d7',
+        issuer: 'https://dev-54990414.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
